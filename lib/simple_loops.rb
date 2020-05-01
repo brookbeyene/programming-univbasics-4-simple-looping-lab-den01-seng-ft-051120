3f1d6eec4bf8545daea22ded98b3a9f2b6dae365
Brook Beyene <brookbeyene37@gmail.com>
2020-05-01 04:11:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,2 +1,17 @@
 # Write your methods here
-def loop_message_five_times
+def loop_message_five_times(message)
+  count = 0 
+  while count < 5 do
+    puts message
+    count +=1 
+  end
+  
+end 
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do
+    puts message
+    count +=1 
+  end
+end
